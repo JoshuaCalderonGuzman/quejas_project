b7fb5c2f3e63d0a9e5191c24690786fb3fa3745b
Joshua Calderon Guzman <s22120150@alumnos.itsur.edu.mx>
2025-10-08 00:56:22 -0600

    Creamos el archivo html nueva_queja

--- quejas_app/urls.py
+++ quejas_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.menu_principal, name='menu'),
+    path('nueva-queja/', views.nueva_queja, name='nueva_queja'),
 ]
--- quejas_app/views.py
+++ quejas_app/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def menu_principal(request):
     return render(request, 'menu.html')
+
+def nueva_queja(request):
+    if request.method == 'POST':
+        pass
+    return render(request, 'nueva_queja.html')
