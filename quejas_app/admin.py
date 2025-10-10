3ce2017e0f2a333ed18b44f48471c15041a6cb49
RobeGGs111 <s22120166@alumnos.itsur.edu.mx>
2025-10-09 22:11:59 -0600

    Se crearon los modelos para todos los elementos de las quejas

--- quejas_app/admin.py
+++ quejas_app/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ('name', 'description')
+	search_fields = ('name',)
+
+
+@admin.register(models.Complaint)
+class ComplaintAdmin(admin.ModelAdmin):
+	list_display = ('title', 'category', 'status', 'reporter_name', 'created_at')
+	list_filter = ('status', 'category', 'created_at')
+	search_fields = ('title', 'description', 'reporter_name', 'reporter_email')
+	readonly_fields = ('created_at', 'updated_at')
+
+
+@admin.register(models.Comment)
+class CommentAdmin(admin.ModelAdmin):
+	list_display = ('complaint', 'author', 'public', 'created_at')
+	list_filter = ('public', 'created_at')
+	search_fields = ('author', 'message')
+
+
+@admin.register(models.Attachment)
+class AttachmentAdmin(admin.ModelAdmin):
+	list_display = ('complaint', 'file', 'uploaded_at')
+	readonly_fields = ('uploaded_at',)
